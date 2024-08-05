54b85e84a565df9a64dbdcd51c28abb8a3d94031
jharry444 <jharrison1@qa.com>
2024-08-05 12:19:59 +0100

    init commit

---
+++ src/main/java/com/qa/sky/spring/Sky3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.qa.sky.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sky3SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sky3SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/sky/spring/Sky3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.sky.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sky3SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
