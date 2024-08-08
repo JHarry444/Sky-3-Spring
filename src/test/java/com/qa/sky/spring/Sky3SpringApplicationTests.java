0494cbffb0d95ac82da6823077a652b4d051e487
jharry444 <jharrison1@qa.com>
2024-08-08 15:02:22 +0100

    profiles

--- src/test/java/com/qa/sky/spring/Sky3SpringApplicationTests.java
+++ src/test/java/com/qa/sky/spring/Sky3SpringApplicationTests.java
@@ -2,8 +2,10 @@ package com.qa.sky.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sky3SpringApplicationTests {
 
 	@Test
--- src/test/java/com/qa/sky/spring/rest/PersonControllerIntegrationTest.java
+++ src/test/java/com/qa/sky/spring/rest/PersonControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -24,6 +25,7 @@ import java.util.List;
 @AutoConfigureMockMvc // creates the mockmvc object
 @Sql(scripts = {"classpath:test-schema.sql", "classpath:test-data.sql"},
         executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
 public class PersonControllerIntegrationTest {
 
     @Autowired // requests the object from spring
